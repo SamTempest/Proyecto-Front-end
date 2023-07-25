73fe156315e9e8cbd391fe44003286a6281371cd
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2023-07-25 18:16:22 -0400

    actualización de guardar formulario

--- Back-end/routes/constructor.r.js
+++ Back-end/routes/constructor.r.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var constructorC=require('../controllers/constructor.c')
 
 /* GET users listing. */
-router.post('/', constructorC.save, function(req, res, next) {
+router.post('/', constructorC.guardar, function(req, res, next) {
   res.status("200").send("llega")
 });
 
