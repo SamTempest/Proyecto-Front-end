653a979a45e5d62a338ca4095f7d3ec40f6df75d
rossjbm <118684575+rossjbm@users.noreply.github.com>
2023-07-25 02:26:10 -0400

    .

--- Back-end/controllers/registro.c.js
+++ Back-end/controllers/registro.c.js
@@ -1,4 +1,5 @@
 const registroModels = require("../models/registro.m")
+const bcryptjs = require('bcryptjs');
 
 class registroControllers {
     
@@ -7,6 +8,9 @@ class registroControllers {
         console.log(req.body);
         const { usuario_unico , contrasena , nombre_completo , correo } = req.body
         const datos = { usuario_unico , contrasena , nombre_completo , correo }
+
+
+
         next()
     }
 }
