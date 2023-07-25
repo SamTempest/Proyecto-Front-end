a640b0a636d7dc1d47a4208dc7b2979b8db17740
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2023-07-25 07:06:27 -0400

    Agregado manejo de constructor

--- Back-end/app.js
+++ Back-end/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var pruebaRouter = require('./routes/prueba.r');
+var homeRouter = require('./routes/home')
+var constructorRouter = require('./routes/constructor.r')
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/prueba', pruebaRouter);
+app.use('/home', homeRouter)
+app.use('/constructor', constructorRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Back-end/controllers/builder.c.js
@@ -0,0 +1,9 @@
+const async = require("hbs/lib/async");
+
+class builder{
+    async save(){
+        
+    }
+
+
+}
