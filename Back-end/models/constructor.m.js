53803597d75719cff522332650e8e535e6592ce3
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2023-07-25 08:04:44 -0400

    actualización promesas
    para que el equipo vea como se maneja las promesas

---
+++ Back-end/models/constructor.m.js
@@ -0,0 +1,19 @@
+const conectandoDB = require("../connection/conexion");
+
+class Constructor {
+  async enviar_a_base() {
+    try {
+      // Query con promesas
+      const [rows] = await conectandoDB.promise().query(
+        "SELECT * FROM formularios"
+      );
+
+      console.log(rows); // resultados
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+module.exports = Constructor;
