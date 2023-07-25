b16bebab365da00d7f5bef918ef9dcb054853df8
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2023-07-25 07:33:39 -0400

    actualización

--- Back-end/routes/constructor.r.js
+++ Back-end/routes/constructor.r.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var constructorC=require('../controllers/constructor.c')
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  console.log(req.body);
+router.post('/', constructorC.save, function(req, res, next) {
+  res.status("200").send("llega")
 });
 
 module.exports = router;
