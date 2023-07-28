9a56aa98767ce1f7cf30959693edc84a371c6bfe
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2023-07-28 16:19:55 -0400

    acomodando la vadilación y empezando la conexión final

--- Back-end/routes/constructor.r.js
+++ Back-end/routes/constructor.r.js
@@ -4,11 +4,7 @@ var router = express.Router();
 let verificador = require('../middlewares/Seccion.mid')
 var constructorC=require('../controllers/constructor.c')
 
-router.get('/', constructorC.verTodo, function(req, res, next) {
-  res.status("200").json({"perfecto":"llego"})
-});
-
-router.post('/', constructorC.guardar, function(req, res, next) {
+router.post('/', verificador.seccion ,constructorC.guardar, function(req, res, next) {
   res.status("200").json({"perfecto":"llego"})
 });
 
